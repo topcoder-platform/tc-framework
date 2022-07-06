b1f8e9e7493d373f48e5eb4a140712105d7bba0d
Marios Kranitsas <marioskranitsas1999@gmail.com>
2022-07-06 16:53:36 +0300

    Implement logging library with APM capabilities

--- src/lib/logger/tracer.js
+++ src/lib/logger/tracer.js
@@ -1,36 +1,33 @@
-const opentelemetry = require('@opentelemetry/api');
-const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-const { SimpleSpanProcessor, ConsoleSpanExporter, BatchSpanProcessor } = require("@opentelemetry/sdk-trace-base");
-const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto');
-// const { registerInstrumentations } = require('@opentelemetry/instrumentation');
-// const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
-const { Resource } = require('@opentelemetry/resources');
-
+const {trace} = require('@opentelemetry/api')
+const { SimpleSpanProcessor, ConsoleSpanExporter, BatchSpanProcessor, BasicTracerProvider } = require("@opentelemetry/sdk-trace-base")
+const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto')
+const { registerInstrumentations } = require('@opentelemetry/instrumentation')
+const { Resource } = require('@opentelemetry/resources')
+const { WinstonInstrumentation } = require('@opentelemetry/instrumentation-winston')
+const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions')
 
 module.exports = (config) => {
- const resources = new Resource({
-    'service.name': config.Service_NAME,
-    'application': config.APPLICATION_NAME,
-    //'ANY_OTHER_ATTRIBUTE_KEY': 'ANY_OTHER_ATTRIBUTE_VALUE',
- }); 
-
- const provider = new NodeTracerProvider({ resource: resources })
  
- const exporterOptions = {
-   url: config.EXPORTER_URL,
- }
+ const exporter = new OTLPTraceExporter({
+  url: config.APM_OTLP_TRACE_EXPORTER_URL
+ })
+
+ const provider = new BasicTracerProvider({
+  resource: new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: config.APM_SERVICE_NAME,
+  }),
+})
+provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
+provider.addSpanProcessor(new BatchSpanProcessor(new BatchSpanProcessor(exporter)))
+provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()))
+provider.register()
+
+registerInstrumentations({
+  instrumentations: [
+    new WinstonInstrumentation(),
+  ],
+})
 
 
- const exporter = new OTLPTraceExporter(exporterOptions);
- provider.addSpanProcessor(new SimpleSpanProcessor(exporter))
- provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
- provider.addSpanProcessor(new BatchSpanProcessor(new exporter()))
- provider.register();
-
-//  registerInstrumentations({
-//   instrumentations: [
-//     new HttpInstrumentation(),
-//   ],
-//  })
- return opentelemetry.trace.getTracer("instrumentation-example");
+ return trace.getTracer(config.APM_TRACER_NAME)
 }
